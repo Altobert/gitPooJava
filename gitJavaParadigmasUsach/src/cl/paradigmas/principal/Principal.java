a258ff10631ed5b28e0c2857f47dbc27a4aeb8ef
did <coalme@gmail.com>
2020-07-20 23:21:36 -0400

    Primer commit de prueba

---
+++ gitJavaParadigmasUsach/src/cl/paradigmas/principal/Principal.java
@@ -0,0 +1,10 @@
+package cl.paradigmas.principal;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola mundo desde eclipse lubuntu");
+	}
+
+}
