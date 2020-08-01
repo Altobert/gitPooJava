7be5cc1d86d4bbcef50c632d751985d555b25748
did <coalme@gmail.com>
2020-07-31 23:52:27 -0400

    se agrega validcion case para opciones menu

--- gitJavaParadigmasUsach/src/cl/paradigmas/principal/Principal.java
+++ gitJavaParadigmasUsach/src/cl/paradigmas/principal/Principal.java
@@ -2,16 +2,17 @@ package cl.paradigmas.principal;
 
 public class Principal {
 	
+	
 	private static String menuAplicacion() {
 		StringBuilder sb = new StringBuilder();
 		sb.append("### SIMULACIÓN DE GIT ###\n");
-		sb.append("Escoja su opción:\n");
-		sb.append("1. add \n");
-		sb.append("2. commit \n");
-		sb.append("3. pull \n");
-		sb.append("4. push \n");
-		sb.append("5. status \n");
-		sb.append("6. Crear nuevo archivo\n");
+		sb.append("Escoja su opción:     	\n");
+		sb.append("1. add                	\n");
+		sb.append("2. commit             	\n");
+		sb.append("3. pull               	\n");
+		sb.append("4. push               	\n");
+		sb.append("5. status             	\n");
+		sb.append("6. Crear nuevo archivo	\n");
 		sb.append("INTRODUZCA SU OPCIÓN: _");
 		return sb.toString();
 	}
@@ -19,6 +20,30 @@ public class Principal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println(menuAplicacion());
+		Integer opcion = 1;
+		switch (opcion) {
+		case 1:
+			
+			break;
+		case 2:
+			
+			break;
+		case 3:
+			
+			break;
+		case 4:
+			
+			break;
+		case 5:
+			
+			break;
+		case 6:
+			
+			break;	
+
+		default:
+			break;
+		}
 	}
 
 }
